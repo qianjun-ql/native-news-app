8e4fae1d8502a3b48158fec0852e14be221fbdc7
Cheryl-LL <qianjunliang.ql@gmail.com>
2024-11-02 22:19:01 -0600

    update slide item

--- components/BreakingNews.tsx
+++ components/BreakingNews.tsx
@@ -1,30 +1,46 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Animated } from "react-native";
 import React, { useState } from "react";
 import { Colors } from "@/constants/Colors";
 import { NewsDataType } from "@/types";
 import SliderItem from "./SliderItem";
+import {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 
 type Props = {
   newsList: Array<NewsDataType>;
 };
 
 const BreakingNews = ({ newsList }: Props) => {
+  const [data, setData] = useState(newsList);
+  const [paginationIndex, setPaginationIndex] = useState(0);
+  const scrollX = useSharedValue(0);
+  const ref = useAnimatedRef<Animated.FlatList<any>>();
 
-  const [data, setData] = useState(newsList)
+  const onScrollHander = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      scrollX.value = e.contentOffset.x;
+    },
+  });
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Breaking News</Text>、
       <View style={styles.slideWrapper}></View>
-      <FlatList
+      <Animated.FlatList
+        ref={ref}
         data={data}
         keyExtractor={(_, index) => `list_item${index}`}
         renderItem={({ item, index }) => (
-          <SliderItem slideItem={item} index={index} />
+          <SliderItem slideItem={item} index={index} srcollX={scrollX}/>
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
         pagingEnabled
+        onScroll={onScrollHander}
+        scrollEventThrottle={16}
       />
     </View>
   );
--- components/SliderItem.tsx
+++ components/SliderItem.tsx
@@ -1,19 +1,38 @@
 import { View, Text, Dimensions, StyleSheet, Image } from "react-native";
 import React from "react";
 import { NewsDataType } from "@/types";
+import { SharedValue } from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
 
 type Props = {
   slideItem: NewsDataType;
   index: number;
+  srcollX: SharedValue<number>;
 };
 
 const { width } = Dimensions.get("screen");
 
-const SliderItem = ({ slideItem, index }: Props) => {
+const SliderItem = ({ slideItem, index, srcollX }: Props) => {
   return (
     <View style={styles.itemWrapper}>
       <Image source={{ uri: slideItem.image_url }} style={styles.image} />
-      <Text>{slideItem.title}</Text>
+      <LinearGradient
+        colors={["transparent", "rgba(0,0,0,0.8"]}
+        style={styles.background}
+      >
+        <View>
+          <View>
+            {slideItem.source_icon && (
+              <Image
+                source={{ uri: slideItem.source_icon }}
+                style={styles.sourceIcon}
+              />
+            )}
+          </View>
+
+          <Text>{slideItem.title}</Text>
+        </View>
+      </LinearGradient>
     </View>
   );
 };
@@ -28,8 +47,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   image: {
-    width: width,
+    width: width - 60,
     height: 180,
     borderRadius: 20,
   },
+  background: {
+    position: "absolute",
+    // left: 30,
+    top: 0,
+    right: 0,
+    width: width - 60,
+    height: 180,
+    borderRadius: 20,
+    padding: 20,
+  },
+  sourceIcon: {},
 });
